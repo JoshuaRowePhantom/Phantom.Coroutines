f2974d667280a364929a861d0a0cf4a0a8995aaf
Joshua Rowe <j.git@phantom.to>
2021-09-18 14:01:34 -0700

    First go at "task"

--- Phantom.Coroutines/include/Phantom.Coroutines/task.h
+++ Phantom.Coroutines/include/Phantom.Coroutines/task.h
@@ -2,71 +2,111 @@
 
 #include "detail/coroutine.h"
 #include "detail/promise_traits.h"
+#include "single_consumer_promise.h"
+#include <concepts>
+#include <exception>
+#include <type_traits>
 #include <variant>
 
 namespace Phantom::Coroutines
 {
-//
-//template<
-//    typename TPromise,
-//    typename TFuture
-//>
-//class task_promise_base
-//    :
-//    protected promise_traits<TPromise>
-//{
-//public:
-//    auto initial_suspend()
-//    {
-//        return suspend_always;
-//    }
-//
-//    auto final_suspend()
-//    {
-//        return suspend_always;
-//    }
-//
-//    auto get_return_object()
-//    {
-//        return TFuture(
-//            promise_traits::get_coroutine_handle()
-//        );
-//    }
-//};
-//
-//template<
-//    typename TPromise,
-//    typename TFuture
-//>
-//class task_promise
-//    :
-//    public TPromise
-//{
-//    using TPromise::TPromise;
-//};
-//
-//template<
-//    typename TResult = void,
-//    typename TPromise = task_promise<TResult>
-//>
-//class [[nodiscard]] task
-//    :
-//    protected promise_traits<TPromise>
-//{
-//public:
-//    typedef TPromise promise_type;
-//    typedef coroutine_handle<promise_type> coroutine_handle_type;
-//    typedef TResult result_type;
-//
-//private:
-//    coroutine_handle_type m_coroutineHandle;
-//
-//public:
-//    task(
-//        coroutine_handle_type coroutineHandle = coroutine_handle_type()
-//    ) : m_coroutineHandle(
-//        coroutineHandle)
-//    {}
-//};
+namespace detail
+{
+template<
+    typename TValue
+>
+class task
+{
+    typedef std::conditional_t<
+        std::is_same_v<void, TValue>,
+        std::monostate,
+        TValue
+    > variant_member_type;
+
+    typedef std::variant<
+        variant_member_type,
+        std::exception_ptr
+    > promise_value_type;
+
+    const size_t value_index = 0;
+    const size_t exception_index = 1;
+
+    class promise_type
+    {
+        friend class task;
+
+        single_consumer_promise<promise_value_type> m_singleConsumerPromise;
+
+        suspend_always initial_suspend() const { return suspend_always{}; }
+        suspend_never final_suspend() const { return suspend_never{}; }
+        task get_return_object() { return task{ *this }; }
+
+        std::enable_if_t<std::same_as<TValue, void>> return_void()
+        {
+            m_singleConsumerPromise.emplace(
+                std::in_place_index<value_index>,
+                std::monostate
+            );
+        }
+
+        template<
+            typename TResult
+        >
+        std::enable_if_t<!std::same_as<TValue, void>> return_value(
+            TResult&& result
+        )
+        {
+            m_singleConsumerPromise.emplace(
+                std::in_place_index<value_index>,
+                std::forward<TResult>(result)
+            );
+        }
+
+        void unhandled_exception()
+        {
+            m_singleConsumerPromise.emplace(
+                std::in_place_index<exception_index>,
+                std::current_exception()
+            );
+        }
+    };
+
+    promise_type* m_promise;
+
+    task(
+        promise_type* promise
+    ) : m_promise(
+        promise
+    )
+    {}
+
+public:
+    bool await_ready() { return m_promise->m_singleConsumerPromise.await_ready(); }
+    
+    bool await_suspend(
+        coroutine_handle<> coroutine
+    )
+    {
+        return m_promise->m_singleConsumerPromise.await_suspend(
+            coroutine
+        );
+    }
+
+    TValue& await_resume()
+    {
+        auto& value = m_promise->m_singleConsumerPromise.await_resume();
+        
+        if (value.index() == value_index)
+        {
+            return get<value_index>(value);
+        }
+
+        std::rethrow_exception(
+            get<exception_index>(value));
+    }
+};
+
+}
+using detail::task;
 
 }
