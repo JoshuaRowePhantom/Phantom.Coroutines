13d0844a30f4f68efe3c26602f098c5659ef852a
Joshua Rowe <j.git@phantom.to>
2021-09-20 18:06:43 -0700

    Simplify sync_wait Move the promise object to the async method

--- Phantom.Coroutines/include/Phantom.Coroutines/sync_wait.h
+++ Phantom.Coroutines/include/Phantom.Coroutines/sync_wait.h
@@ -8,106 +8,30 @@ namespace Phantom::Coroutines
 namespace detail
 {
 
-
-template<
-    is_awaitable TAwaitable,
-    typename TResult
->
 struct as_future_implementation_promise;
 
-template<
-    is_awaitable TAwaitable,
-    typename TResult
->
 struct as_future_implementation_awaitable
 {
-    typedef as_future_implementation_promise<TAwaitable, TResult> promise_type;
+    typedef as_future_implementation_promise promise_type;
 };
 
-template<
-    is_awaitable TAwaitable,
-    typename TResult
->
-struct as_future_implementation_promise_base
+struct as_future_implementation_promise
 {
-    std::promise<TResult>& m_promise;
-    std::promise<TResult>& promise() { return m_promise; }
-
-    typedef as_future_implementation_promise<TAwaitable, TResult> promise_type;
-
-    auto get_coroutine_handle()
-    {
-        return coroutine_handle<promise_type>::from_promise(
-            static_cast<promise_type&>(*this));
-    }
-
-    as_future_implementation_promise_base(
-        std::promise<TResult>& promise,
-        TAwaitable&
-    ) :
-        m_promise{ promise }
-    {
-    }
-
     constexpr suspend_never initial_suspend() const noexcept { return suspend_never{}; }
     constexpr suspend_never final_suspend() const noexcept { return suspend_never{}; }
 
-    constexpr as_future_implementation_awaitable<TAwaitable, TResult> get_return_object() const noexcept
+    constexpr as_future_implementation_awaitable get_return_object() const noexcept
     {
-        return as_future_implementation_awaitable<TAwaitable, TResult>{};
+        return as_future_implementation_awaitable{};
     }
 
-    void unhandled_exception() noexcept
+    constexpr void return_void() noexcept
     {
-        m_promise.set_exception(
-            std::current_exception()
-        );
     }
-};
 
-template<
-    is_awaitable TAwaitable,
-    typename TResult
->
-struct as_future_implementation_promise
-    :
-public as_future_implementation_promise_base<
-    TAwaitable,
-    TResult
->
-{
-    using as_future_implementation_promise::as_future_implementation_promise_base::as_future_implementation_promise_base;
-    using as_future_implementation_promise::as_future_implementation_promise_base::promise;
-
-    template<
-        typename TValue
-    >
-    void return_value(
-        TValue&& value)
+    constexpr void unhandled_exception() noexcept
     {
-        promise().set_value(
-            std::forward<TValue>(value));
-    }
-};
-
-template<
-    is_awaitable TAwaitable
->
-struct as_future_implementation_promise<
-    TAwaitable,
-    void
-> :
-public as_future_implementation_promise_base<
-    TAwaitable,
-    void
->
-{
-    using as_future_implementation_promise::as_future_implementation_promise_base::as_future_implementation_promise_base;
-    using as_future_implementation_promise::as_future_implementation_promise_base::promise;
-
-    void return_void()
-    {
-        promise().set_value();
+        assert(false);
     }
 };
 
@@ -115,22 +39,30 @@ template<
     is_awaitable TAwaitable,
     typename TResult
 >
-as_future_implementation_awaitable<
-    TAwaitable,
-    TResult
->
+as_future_implementation_awaitable
 as_future_implementation(
-    std::promise<TResult>& promise,
+    std::promise<TResult> promise,
     TAwaitable&& awaitable
 )
 {
-    if constexpr (std::is_same_v<TResult, void>)
+    try
     {
-        co_await awaitable;
+        if constexpr (std::is_same_v<TResult, void>)
+        {
+            co_await awaitable;
+            promise.set_value();
+        }
+        else
+        {
+            promise.set_value(
+                co_await awaitable);
+        }
     }
-    else
+    catch (...)
     {
-        co_return co_await awaitable;
+        promise.set_exception(
+            std::current_exception()
+        );
     }
 }
 
@@ -147,7 +79,7 @@ template<
     auto future = promise.get_future();
 
     as_future_implementation(
-        promise,
+        std::move(promise),
         std::forward<TAwaitable>(awaitable));
 
     return future;
