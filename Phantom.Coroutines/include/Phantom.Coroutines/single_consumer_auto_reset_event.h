798e5175ba11bc5ecbe3a2c3e1dc55936d4b93fb
Joshua Rowe <j.git@phantom.to>
2021-11-25 10:14:25 -0800

    Refactored single_consumer_xyz_reset_event into single_consumer_event base template class

--- Phantom.Coroutines/include/Phantom.Coroutines/single_consumer_auto_reset_event.h
+++ Phantom.Coroutines/include/Phantom.Coroutines/single_consumer_auto_reset_event.h
@@ -1,129 +1,19 @@
 #pragma once
 
-#include <atomic>
-#include "detail/atomic_state.h"
-#include "detail/coroutine.h"
+#include "detail/single_consumer_event.h"
 
 namespace Phantom::Coroutines
 {
 namespace detail
 {
+
 class single_consumer_auto_reset_event
+    :
+    public single_consumer_event<
+    single_consumer_event_states::NotSignalledState
+    >
 {
-    struct NotSignalledState {};
-    struct SignalledState {};
-    struct WaitingCoroutineState;
-
-    typedef atomic_state<
-        SingletonState<NotSignalledState>,
-        SingletonState<SignalledState>,
-        StateSet<WaitingCoroutineState, coroutine_handle<>>
-    > atomic_state_type;
-
-    atomic_state_type m_atomicState;
-    typedef state<atomic_state_type> state_type;
-
-public:
-    single_consumer_auto_reset_event(
-        bool initiallySignalled = false
-    ) : m_atomicState(
-        initiallySignalled 
-        ? 
-        state_type{ SignalledState{} }
-        : 
-        state_type{ NotSignalledState{} }
-    )
-    {}
-
-    ~single_consumer_auto_reset_event()
-    {
-        assert(!m_atomicState.load(std::memory_order_acquire).is<WaitingCoroutineState>());
-    }
-
-    bool is_set() const
-    {
-        return m_atomicState.load(std::memory_order_acquire).is<SignalledState>();
-    }
-
-    void set()
-    {
-        auto nextStateLambda = [&](auto previousState) -> state_type
-        {
-            if (previousState.is<WaitingCoroutineState>())
-            {
-                return NotSignalledState{};
-            }
-            return SignalledState{};
-        };
-
-        auto previousState = compare_exchange_weak_loop(
-            m_atomicState,
-            nextStateLambda,
-            std::memory_order_relaxed
-        );
-
-        if (previousState.is<WaitingCoroutineState>())
-        {
-            auto coroutine = previousState.as<WaitingCoroutineState>();
-            coroutine.resume();
-        }
-    }
-
-    void reset()
-    {
-        state_type expectedState = SignalledState{};
-
-        // If the state was Signalled, it becomes NotSignalled.
-        // If the state was NotSignalled, it stays that way.
-        // If the state was WaitingCoroutine, it stays that way.
-        m_atomicState.compare_exchange_strong(
-            expectedState,
-            NotSignalledState{},
-            std::memory_order_acq_rel,
-            std::memory_order_acquire);
-    }
-
-    bool await_ready() noexcept
-    {
-        return m_atomicState.load(std::memory_order_acquire) == SignalledState{};
-    }
-
-    bool await_suspend(
-        coroutine_handle<> coroutine
-    ) noexcept
-    {
-        auto nextStateLambda = [&](auto previousState) -> state_type
-        {
-            if (previousState == SignalledState{})
-            {
-                return NotSignalledState{};
-            }
-            return coroutine;
-        };
-
-        auto previousState = compare_exchange_weak_loop(
-            m_atomicState,
-            nextStateLambda,
-            std::memory_order_relaxed
-        );
-
-        if (previousState == SignalledState{})
-        {
-            return false;
-        }
-
-        // If there is another coroutine handle,
-        // then this object isn't being used as expected;
-        // there's only supposed to be a single consumer ever doing
-        // co_await operations.
-        assert(previousState == NotSignalledState{});
-
-        return true;
-    };
-
-    void await_resume() noexcept
-    {
-    }
+    using single_consumer_auto_reset_event::single_consumer_event::single_consumer_event;
 };
 
 }
--- Phantom.Coroutines/include/Phantom.Coroutines/single_consumer_manual_reset_event.h
+++ Phantom.Coroutines/include/Phantom.Coroutines/single_consumer_manual_reset_event.h
@@ -1,9 +1,6 @@
 #pragma once
 
-#include <atomic>
-#include "detail/atomic_state.h"
-#include "detail/coroutine.h"
-#include "detail/immovable_object.h"
+#include "detail/single_consumer_event.h"
 
 namespace Phantom::Coroutines
 {
@@ -11,137 +8,11 @@ namespace detail
 {
 class single_consumer_manual_reset_event
     :
-private immovable_object
+public single_consumer_event<
+    single_consumer_event_states::SignalledState
+>
 {
-    struct NotSignalledState {};
-    struct SignalledState {};
-    struct WaitingCoroutineState;
-
-    typedef atomic_state<
-        SingletonState<NotSignalledState>,
-        SingletonState<SignalledState>,
-        StateSet<WaitingCoroutineState, coroutine_handle<>>
-    > atomic_state_type;
-
-    atomic_state_type m_atomicState;
-    typedef state<atomic_state_type> state_type;
-public:
-    single_consumer_manual_reset_event(
-        bool initiallySignalled = false
-    ) : m_atomicState(
-        initiallySignalled
-        ?
-        state_type{ SignalledState{} }
-        :
-        state_type{ NotSignalledState{} }
-    )
-    {}
-
-#if 0
-    ~single_consumer_manual_reset_event()
-    {
-        // It's okay to tear down a set of objects that included suspended things.
-        // assert(!m_atomicState.load(std::memory_order_acquire).is<WaitingCoroutineState>());
-    }
-#endif
-
-    bool is_set() const
-    {
-        return m_atomicState.load(std::memory_order_acquire).is<SignalledState>();
-    }
-
-    void set()
-    {
-        auto previousState = m_atomicState.exchange(
-            state_type{ SignalledState{} },
-            std::memory_order_acq_rel
-        );
-
-        if (previousState.is<WaitingCoroutineState>())
-        {
-            auto coroutine = previousState.as<WaitingCoroutineState>();
-            coroutine.resume();
-        }
-    }
-
-    void reset()
-    {
-        state_type expectedState = SignalledState{};
-
-        // If the state was Signalled, it becomes NotSignalled.
-        // If the state was NotSignalled, it stays that way.
-        // If the state was WaitingCoroutine, it stays that way.
-        m_atomicState.compare_exchange_strong(
-            expectedState,
-            NotSignalledState{},
-            std::memory_order_acq_rel,
-            std::memory_order_acquire);
-    }
-
-    class awaiter
-        :
-    private immovable_object
-    {
-        friend class single_consumer_manual_reset_event;
-        single_consumer_manual_reset_event& m_event;
-
-        awaiter(
-            single_consumer_manual_reset_event& event
-        ) :
-            m_event
-        {
-            event
-        }
-        {}
-
-    public:
-        bool await_ready() noexcept
-        {
-            return m_event.m_atomicState.load(std::memory_order_acquire) == SignalledState{};
-        }
-
-        bool await_suspend(
-            coroutine_handle<> coroutine
-        ) noexcept
-        {
-            auto nextStateLambda = [&](auto previousState) -> state_type
-            {
-                if (previousState == NotSignalledState{})
-                {
-                    return coroutine;
-                }
-                return SignalledState{};
-            };
-
-            auto previousState = compare_exchange_weak_loop(
-                m_event.m_atomicState,
-                nextStateLambda,
-                std::memory_order_relaxed
-            );
-
-            if (previousState == SignalledState{})
-            {
-                return false;
-            }
-
-            // If there is another coroutine handle,
-            // then this object isn't being used as expected;
-            // there's only supposed to be a single consumer ever doing
-            // co_await operations.
-            assert(previousState == NotSignalledState{});
-
-            return true;
-        };
-
-        void await_resume() noexcept
-        {
-        }
-    };
-
-    awaiter operator co_await() 
-    {
-        return awaiter{ *this }; 
-    }
+    using single_consumer_manual_reset_event::single_consumer_event::single_consumer_event;
 };
 
 }
