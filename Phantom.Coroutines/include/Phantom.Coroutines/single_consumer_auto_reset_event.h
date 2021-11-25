1560ca4fd351a530a658c4609b53b91351a0e4e4
Joshua Rowe <j.git@phantom.to>
2021-11-25 10:17:07 -0800

    Formatting

--- Phantom.Coroutines/include/Phantom.Coroutines/single_consumer_auto_reset_event.h
+++ Phantom.Coroutines/include/Phantom.Coroutines/single_consumer_auto_reset_event.h
@@ -9,9 +9,9 @@ namespace detail
 
 class single_consumer_auto_reset_event
     :
-    public single_consumer_event<
+public single_consumer_event<
     single_consumer_event_states::NotSignalledState
-    >
+>
 {
     using single_consumer_auto_reset_event::single_consumer_event::single_consumer_event;
 };
