6a227186631ab9b809fdc0a88dedcda34e3982f5
Joshua Rowe <j.git@phantom.to>
2021-12-08 14:25:41 -0800

    Added more tests

--- Phantom.Coroutines.Test/generator_test.cpp
+++ Phantom.Coroutines.Test/generator_test.cpp
@@ -15,3 +15,35 @@ TEST(generator_test, Can_enumerate_empty_generator)
 
 	ASSERT_EQ(0, count);
 }
+
+TEST(generator_test, Can_enumerate_non_empty_iterator)
+{
+	auto myGenerator = []()->generator<int> 
+	{ 
+		co_yield 1;
+		co_yield 2;
+		co_yield 3;
+	}();
+
+	auto iterator = myGenerator.begin();
+	ASSERT_EQ(1, *iterator);
+	ASSERT_EQ(2, *++iterator);
+	ASSERT_EQ(3, *++iterator);
+	ASSERT_EQ(generator<int>::iterator_type{}, ++iterator);
+}
+
+TEST(generator_test, Can_enumerate_non_empty_iterator_with_co_return)
+{
+	auto myGenerator = []()->generator<int>
+	{
+		co_yield 1;
+		co_yield 2;
+		co_return;
+		co_yield 3;
+	}();
+
+	auto iterator = myGenerator.begin();
+	ASSERT_EQ(1, *iterator);
+	ASSERT_EQ(2, *++iterator);
+	ASSERT_EQ(generator<int>::iterator_type{}, ++iterator);
+}
