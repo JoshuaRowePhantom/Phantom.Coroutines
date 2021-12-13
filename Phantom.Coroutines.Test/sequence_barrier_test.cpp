3d2e17daa0f1dac5d4a86e4415d48e0004660b39
Joshua Rowe <j.git@phantom.to>
2021-12-13 11:36:16 -0800

    Assert something about sequence_barrier

--- Phantom.Coroutines.Test/sequence_barrier_test.cpp
+++ Phantom.Coroutines.Test/sequence_barrier_test.cpp
@@ -3,3 +3,4 @@
 using namespace Phantom::Coroutines;
 using namespace Phantom::Coroutines::detail;
 
+static_assert(std::same_as<std::size_t, awaitable_result_type_t<decltype(std::declval<sequence_barrier<size_t>>().wait_for(0))>>);
