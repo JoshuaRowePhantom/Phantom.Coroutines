be0a192e9adee307fad4a09146cc759a45c10583
Joshua Rowe <j.git@phantom.to>
2022-03-13 22:00:13 -0700

    Added test inline_scheduler_test, schedules_on_current_thread

--- Phantom.Coroutines.Test/inline_scheduler_test.cpp
+++ Phantom.Coroutines.Test/inline_scheduler_test.cpp
@@ -1,6 +1,17 @@
+#include "async_test.h"
 #include "Phantom.Coroutines/inline_scheduler.h"
+#include <thread>
 
 using namespace Phantom::Coroutines;
 using namespace Phantom::Coroutines::detail;
 
 static_assert(scheduler<inline_scheduler>);
+
+ASYNC_TEST(inline_scheduler_test, schedules_on_current_thread)
+{
+	inline_scheduler scheduler;
+	auto currentThreadId = std::this_thread::get_id();
+	co_await scheduler;
+	auto invokedThreadId = std::this_thread::get_id();
+	EXPECT_EQ(currentThreadId, invokedThreadId);
+}
