458b8d88018665336d4f33c2241ae5c8fcd871fb
Joshua Rowe <j.git@phantom.to>
2022-04-18 20:32:23 -0700

    Add cppcoro compatibility layer

---
+++ Phantom.Coroutines.cppcoro.Test/async_auto_reset_event_test.cpp
@@ -0,0 +1,1 @@
+#include "cppcoro/async_auto_reset_event.hpp"
---
+++ Phantom.Coroutines.cppcoro/include/cppcoro/async_auto_reset_event.hpp
@@ -0,0 +1,5 @@
+#include "Phantom.Coroutines/auto_reset_event.h"
+namespace cppcoro
+{
+typedef ::Phantom::Coroutines::auto_reset_event async_auto_reset_event;
+}
