adaed781adcaa352b9ab4f48088c8ee28738a986
Joshua Rowe <j.git@phantom.to>
2022-04-19 07:03:07 -0700

    Add cppcoro compatibility for async_generator

---
+++ Phantom.Coroutines.cppcoro.Test/async_generator_test.cpp
@@ -0,0 +1,1 @@
+#include "cppcoro/async_generator.hpp"
---
+++ Phantom.Coroutines.cppcoro/include/cppcoro/async_generator.hpp
@@ -0,0 +1,9 @@
+#include "Phantom.Coroutines/async_generator.h"
+
+namespace cppcoro
+{
+template<
+	typename T
+>
+using async_generator = ::Phantom::Coroutines::async_generator<T>;
+}
