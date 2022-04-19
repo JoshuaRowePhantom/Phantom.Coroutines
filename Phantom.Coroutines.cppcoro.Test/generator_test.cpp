91a5af9f4cd65ed14079a5a1e4f31dbd6655509a
Joshua Rowe <j.git@phantom.to>
2022-04-19 07:19:45 -0700

    Add cppcoro compatibility for generator

---
+++ Phantom.Coroutines.cppcoro.Test/generator_test.cpp
@@ -0,0 +1,1 @@
+#include "cppcoro/generator.hpp"
---
+++ Phantom.Coroutines.cppcoro/include/cppcoro/generator.hpp
@@ -0,0 +1,8 @@
+#include "Phantom.Coroutines/generator.h"
+namespace cppcoro
+{
+template<
+	typename T
+>
+using generator = ::Phantom::Coroutines::generator<T>;
+}
