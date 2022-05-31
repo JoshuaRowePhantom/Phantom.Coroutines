8cc520ea215c93bd8336cb22a0d7f38b25bd77a3
Joshua Rowe <j.git@phantom.to>
2022-05-31 12:44:32 -0700

    Add copy_and_invalidate for debugging purposes

--- Phantom.Coroutines/include/Phantom.Coroutines/detail/coroutine.h
+++ Phantom.Coroutines/include/Phantom.Coroutines/detail/coroutine.h
@@ -17,6 +17,31 @@ static inline auto noop_coroutine()
     return std::noop_coroutine();
 }
 
+#ifndef NDEBUG
+// Create a coroutine handle that is not null but isn't valid either,
+// for debugging purposes.
+static inline auto invalid_coroutine_handle()
+{
+    return coroutine_handle<>::from_address(
+        reinterpret_cast<void*>(0x0cfcfcfcfcfcfcfcULL)
+    );
+}
+#endif
+
+template<
+    typename Promise
+>
+static inline auto copy_and_invalidate(
+    coroutine_handle<Promise>& handle
+)
+{
+    auto result = handle;
+#ifndef NDEBUG
+    handle = invalid_coroutine_handle();
+#endif
+    return result;
+}
+
 }
 
 using detail::coroutine_handle;
