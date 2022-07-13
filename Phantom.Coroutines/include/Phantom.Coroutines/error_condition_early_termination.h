30f5916972abec4aa29e98fea80a884066c04a88
Joshua Rowe <j.git@phantom.to>
2022-07-13 07:24:02 -0700

    More work on error_condition_early_termination_await_transformer

--- Phantom.Coroutines/include/Phantom.Coroutines/error_condition_early_termination.h
+++ Phantom.Coroutines/include/Phantom.Coroutines/error_condition_early_termination.h
@@ -2,6 +2,7 @@
 
 #include "early_termination_task.h"
 #include <system_error>
+#include <variant>
 
 namespace Phantom::Coroutines
 {
@@ -13,7 +14,7 @@ template<
 >
 class error_condition_early_termination_await_transformer
 {
-	class awaiter : basic_early_termination_transformed_awaiter<Promise>
+	class void_awaiter : basic_early_termination_transformed_awaiter<Promise>
 	{
 		template<
 			is_early_termination_promise Promise
@@ -22,7 +23,7 @@ class error_condition_early_termination_await_transformer
 
 		std::error_condition m_errorCondition;
 
-		awaiter(
+		void_awaiter(
 			error_condition_early_termination_await_transformer& transformer,
 			std::error_condition errorCondition
 		) :
@@ -50,15 +51,71 @@ class error_condition_early_termination_await_transformer
 		}
 	};
 
+	template<
+		typename Result
+	>
+	class typed_awaiter : basic_early_termination_transformed_awaiter<Promise>
+	{
+		template<
+			is_early_termination_promise Promise
+		>
+		friend class error_condition_early_termination;
+
+		std::variant<Result, std::error_condition>& m_result;
+
+		typed_awaiter(
+			error_condition_early_termination_await_transformer& transformer,
+			std::variant<Result, std::error_condition>& result
+		) :
+			basic_early_termination_awaiter<Promise>{ transformer },
+			m_result{ std::move(result) }
+		{}
+
+	public:
+		bool await_ready() const noexcept
+		{
+			return m_result.index() == 0;
+		}
+
+		auto await_suspend() noexcept
+		{
+			this->return_value(
+				early_termination_result
+				{ 
+					std::get<std::error_condition>(m_result) 
+				}
+			);
+			return this->resume();
+		}
+
+		decltype(auto) await_resume() const noexcept
+		{
+			return m_result.get<0>();
+		}
+	};
+
 public:
-	awaiter await_transform(
+	void_awaiter await_transform(
 		std::error_condition errorCondition
 	)
 	{
-		return awaiter
+		return void_awaiter
 		{
 			*this,
 			errorCondition
+		};
+	}
+
+	template<
+		typename Result
+	> typed_awaiter<Result> await_transform(
+		std::variant<Result, std::error_condition>&& result
+	)
+	{
+		return typed_awaiter<Result>
+		{
+			*this,
+			result
 		};
 	}
 };
