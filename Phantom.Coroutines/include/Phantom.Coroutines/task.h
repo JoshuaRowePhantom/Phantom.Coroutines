d4577f725dce117d576db4dae021b50ef31aa720
Joshua Rowe <j.git@phantom.to>
2022-12-28 11:41:05 -0800

    Untabify

--- Phantom.Coroutines/include/Phantom.Coroutines/task.h
+++ Phantom.Coroutines/include/Phantom.Coroutines/task.h
@@ -18,253 +18,253 @@ namespace Phantom::Coroutines::detail
 {
 
 template<
-	typename Result,
-	is_continuation Continuation
+    typename Result,
+    is_continuation Continuation
 > class basic_task_promise;
 
 template<
-	typename Policy
+    typename Policy
 > concept is_task_promise_policy =
 is_continuation_type_policy<Policy>;
 
 template<
-	typename Result,
-	is_task_promise_policy ... Policies
+    typename Result,
+    is_task_promise_policy ... Policies
 > 
 using task_promise = basic_task_promise<
-	Result,
-	select_continuation_type<
-		Policies..., 
-	    continuation_type<coroutine_handle<>>>
+    Result,
+    select_continuation_type<
+        Policies..., 
+        continuation_type<coroutine_handle<>>>
 >;
 
 template<
-	typename Promise
+    typename Promise
 > class basic_task;
 
 template<
-	typename Result = void
+    typename Result = void
 > using task = basic_task<task_promise<Result>>;
 
 template<
-	typename Promise
+    typename Promise
 > class task_awaiter;
 
 template<
-	typename Result,
-	is_continuation Continuation
+    typename Result,
+    is_continuation Continuation
 > class basic_task_promise
-	:
-	public variant_return_result<Result>,
-	public extensible_promise,
-	private variant_result_storage<Result>
+    :
+    public variant_return_result<Result>,
+    public extensible_promise,
+    private variant_result_storage<Result>
 {
-	using variant_result_storage<Result>::is_void;
+    using variant_result_storage<Result>::is_void;
 
-	friend class basic_task<basic_task_promise>;
+    friend class basic_task<basic_task_promise>;
 
-	typedef std::variant<
-		std::monostate,
-		typename variant_result_storage<Result>::result_variant_member_type,
-		std::exception_ptr
-	> result_variant_type;
+    typedef std::variant<
+        std::monostate,
+        typename variant_result_storage<Result>::result_variant_member_type,
+        std::exception_ptr
+    > result_variant_type;
 
-	result_variant_type m_result;
-	static const size_t result_index = 1;
-	static const size_t exception_index = 2;
+    result_variant_type m_result;
+    static const size_t result_index = 1;
+    static const size_t exception_index = 2;
 
-	Continuation m_continuation;
+    Continuation m_continuation;
 
 public:
-	typedef Result result_type;
+    typedef Result result_type;
 
-	auto initial_suspend(
-		this auto& self
-	) noexcept
-	{
-		return suspend_always{};
-	}
+    auto initial_suspend(
+        this auto& self
+    ) noexcept
+    {
+        return suspend_always{};
+    }
 
-	auto final_suspend(
-		this auto& self
-	) noexcept
-	{
-		return final_suspend_transfer{ self.m_continuation };
-	}
+    auto final_suspend(
+        this auto& self
+    ) noexcept
+    {
+        return final_suspend_transfer{ self.m_continuation };
+    }
 
-	template<
-		typename Self
-	>
-	auto get_return_object(
-		this Self& self
-	) noexcept
-	{
-		return basic_task<Self>(
-			self.handle()
-			);
-	}
+    template<
+        typename Self
+    >
+    auto get_return_object(
+        this Self& self
+    ) noexcept
+    {
+        return basic_task<Self>(
+            self.handle()
+            );
+    }
 
-	auto await_ready(
-		this auto& self,
-		auto& awaiter
-	)
-	{
-		return false;
-	}
+    auto await_ready(
+        this auto& self,
+        auto& awaiter
+    )
+    {
+        return false;
+    }
 
-	auto await_suspend(
-		this auto& self,
-		auto& awaiter,
-		auto continuation
-	)
-	{
-		self.m_continuation = continuation;
-		return self.handle();
-	}
+    auto await_suspend(
+        this auto& self,
+        auto& awaiter,
+        auto continuation
+    )
+    {
+        self.m_continuation = continuation;
+        return self.handle();
+    }
 
-	decltype(auto) await_resume(
-		this auto& self,
-		auto& awaiter
-	)
-	{
-		if (self.m_result.index() == exception_index)
-		{
-			rethrow_exception(
-				get<exception_index>(
-					self.m_result)
-			);
-		}
+    decltype(auto) await_resume(
+        this auto& self,
+        auto& awaiter
+    )
+    {
+        if (self.m_result.index() == exception_index)
+        {
+            rethrow_exception(
+                get<exception_index>(
+                    self.m_result)
+            );
+        }
 
-		return self.return_result<result_index>(
-			self.m_result);
-	}
+        return self.return_result<result_index>(
+            self.m_result);
+    }
 
-	void return_variant_result(
-		this auto& self,
-		auto&& value
-	)
-	{
-		self.m_result.emplace<result_index>(
-			std::forward<decltype(value)>(value));
-	}
+    void return_variant_result(
+        this auto& self,
+        auto&& value
+    )
+    {
+        self.m_result.emplace<result_index>(
+            std::forward<decltype(value)>(value));
+    }
 
-	void unhandled_exception(
-		this auto& self
-	)
-	{
-		self.m_result.emplace<exception_index>(
-			std::current_exception());
-	}
+    void unhandled_exception(
+        this auto& self
+    )
+    {
+        self.m_result.emplace<exception_index>(
+            std::current_exception());
+    }
 };
 
 template<
-	typename Promise
+    typename Promise
 > class task_awaitable
-	:
+    :
 public extensible_awaitable<Promise>
 {
 protected:
-	task_awaitable(
-	)
-	{}
+    task_awaitable(
+    )
+    {}
 
-	task_awaitable(
-		coroutine_handle<Promise> handle
-	) :
-		extensible_awaitable<Promise>{ handle }
-	{}
+    task_awaitable(
+        coroutine_handle<Promise> handle
+    ) :
+        extensible_awaitable<Promise>{ handle }
+    {}
 
-	task_awaitable(
-		task_awaitable&& other
-	) :
-		extensible_awaitable<Promise>{ std::move(other) }
-	{
-		other.handle() = nullptr;
-	}
+    task_awaitable(
+        task_awaitable&& other
+    ) :
+        extensible_awaitable<Promise>{ std::move(other) }
+    {
+        other.handle() = nullptr;
+    }
 
 public:
-	task_awaitable& operator=(
-		task_awaitable&& other
-		)
-	{
-		if (this->handle())
-		{
-			this->handle().destroy();
-		}
-		this->handle() = other.handle();
-		other.handle() = nullptr;
-	}
+    task_awaitable& operator=(
+        task_awaitable&& other
+        )
+    {
+        if (this->handle())
+        {
+            this->handle().destroy();
+        }
+        this->handle() = other.handle();
+        other.handle() = nullptr;
+    }
 
-	~task_awaitable()
-	{
-		if (this->handle())
-		{
-			this->handle().destroy();
-		}
-	}
+    ~task_awaitable()
+    {
+        if (this->handle())
+        {
+            this->handle().destroy();
+        }
+    }
 };
 
 template<
-	typename Promise
+    typename Promise
 > class task_awaiter 
-	:
+    :
 task_awaitable<Promise>
 {
 public:
-	task_awaiter(
-		task_awaitable<Promise>&& other
-	) :
-		task_awaitable<Promise>{ std::move(other) }
-	{}
+    task_awaiter(
+        task_awaitable<Promise>&& other
+    ) :
+        task_awaitable<Promise>{ std::move(other) }
+    {}
 
-	bool await_ready(
-		this auto& self
-	) noexcept
-	{
-		return self.promise().await_ready(self);
-	}
+    bool await_ready(
+        this auto& self
+    ) noexcept
+    {
+        return self.promise().await_ready(self);
+    }
 
-	auto await_suspend(
-		this auto& self,
-		auto awaiter
-	) noexcept
-	{
-		return self.promise().await_suspend(self, awaiter);
-	}
+    auto await_suspend(
+        this auto& self,
+        auto awaiter
+    ) noexcept
+    {
+        return self.promise().await_suspend(self, awaiter);
+    }
 
-	decltype(auto) await_resume(
-		this auto& self
-	)
-	{
-		return self.promise().await_resume(self);
-	}
+    decltype(auto) await_resume(
+        this auto& self
+    )
+    {
+        return self.promise().await_resume(self);
+    }
 };
 
 template<
-	typename Promise
+    typename Promise
 > class basic_task
-	:
-	task_awaitable<Promise>
+    :
+    task_awaitable<Promise>
 {
 public:
-	basic_task(coroutine_handle<Promise> handle)
-		: task_awaitable<Promise>{ handle }
-	{}
+    basic_task(coroutine_handle<Promise> handle)
+        : task_awaitable<Promise>{ handle }
+    {}
 
-	using promise_type = Promise;
+    using promise_type = Promise;
 
-	basic_task()
-	{}
+    basic_task()
+    {}
 
-	auto operator co_await(
-		this std::movable auto && self
-		)
-	{
-		return task_awaiter<Promise>
-		{
-			std::move(self),
-		};
-	}
+    auto operator co_await(
+        this std::movable auto && self
+        )
+    {
+        return task_awaiter<Promise>
+        {
+            std::move(self),
+        };
+    }
 };
 
 }
