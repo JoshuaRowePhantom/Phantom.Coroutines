517ee035c9ce77684754a9de04734f8528738abd
Joshua Rowe <j.git@phantom.to>
2023-01-15 11:47:12 -0800

    Remove std::move tests, because they are actually dependent upon the underlying promise type.

--- Phantom.Coroutines.Test/async_generator_test.cpp
+++ Phantom.Coroutines.Test/async_generator_test.cpp
@@ -115,63 +115,6 @@ ASYNC_TEST(async_generator_test, Returns_reference_to_original_for_byref_iterato
     EXPECT_EQ(&original, &*iterator);
 }
 
-ASYNC_TEST(async_generator_test, Moved_from_async_generator_via_construction_is_empty)
-{
-    std::string original;
-
-    std::optional<async_generator<int>> myGenerator1 = [&]()->async_generator<int>
-    {
-        co_yield 1;
-        co_yield 2;
-        co_yield 3;
-    }();
-
-    auto iterator = myGenerator1->begin();
-    auto myGenerator2 = std::move(*myGenerator1);
-
-    EXPECT_EQ(co_await myGenerator1->begin(), myGenerator1->end());
-}
-
-ASYNC_TEST(async_generator_test, Moved_from_async_generator_via_assignment_is_empty)
-{
-    std::string original;
-
-    std::optional<async_generator<int>> myGenerator1 = [&]()->async_generator<int>
-    {
-        co_yield 1;
-        co_yield 2;
-        co_yield 3;
-    }();
-
-    auto iterator = myGenerator1->begin();
-    async_generator<int> myGenerator2;
-    myGenerator2 = std::move(*myGenerator1);
-
-    EXPECT_EQ(co_await myGenerator1->begin(), myGenerator1->end());
-}
-
-ASYNC_TEST(async_generator_test, Moving_constructing_async_generator_keeps_iterators_intact)
-{
-    std::string original;
-
-    std::optional<async_generator<int>> myGenerator1 = [&]()->async_generator<int>
-    {
-        co_yield 1;
-        co_yield 2;
-        co_yield 3;
-    }();
-
-    auto iterator = co_await myGenerator1->begin();
-    EXPECT_EQ(1, *iterator);
-    EXPECT_EQ(2, *co_await ++iterator);
-
-    auto myGenerator2 = std::move(*myGenerator1);
-    myGenerator1.reset();
-    EXPECT_EQ(2, *iterator);
-    EXPECT_EQ(3, *co_await ++iterator);
-    EXPECT_EQ(myGenerator2.end(), co_await ++iterator);
-}
-
 ASYNC_TEST(async_generator_test, Can_enumerate_async_actions)
 {
     async_manual_reset_event<> signal1;
