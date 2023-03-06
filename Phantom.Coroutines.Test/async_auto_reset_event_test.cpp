ba2a4249f63d042015bd0bccac287afedb279884
Joshua Rowe <j.git@phantom.to>
2023-03-06 02:52:56 -0800

    Add async_auto_reset_event_test::Many_iterations

--- Phantom.Coroutines.Test/async_auto_reset_event_test.cpp
+++ Phantom.Coroutines.Test/async_auto_reset_event_test.cpp
@@ -2,6 +2,7 @@
 #include "async_test.h"
 #include "Phantom.Coroutines/async_scope.h"
 #include "Phantom.Coroutines/async_auto_reset_event.h"
+#include "Phantom.Coroutines/static_thread_pool.h"
 #include "Phantom.Coroutines/suspend_result.h"
 #include "Phantom.Coroutines/sync_wait.h"
 #include "Phantom.Coroutines/task.h"
@@ -9,40 +10,53 @@
 namespace Phantom::Coroutines
 {
 
-TEST(async_auto_reset_event_test, Can_default_initialize)
+class async_auto_reset_event_test : public ::testing::Test
+{
+protected:
+    static_thread_pool<> waiterPool;
+    static_thread_pool<> setterPool = static_thread_pool<>(std::max(2U, waiterPool.thread_count() / 2));
+};
+
+ASYNC_TEST_F(async_auto_reset_event_test, Can_default_initialize)
 {
     async_auto_reset_event<> event;
+    co_return;
 }
 
-TEST(async_auto_reset_event_test, Starts_as_not_set)
+ASYNC_TEST_F(async_auto_reset_event_test, Starts_as_not_set)
 {
     async_auto_reset_event<> event;
-    ASSERT_FALSE(event.is_set());
+    EXPECT_FALSE(event.is_set());
+    co_return;
 }
 
-TEST(async_auto_reset_event_test, Starts_as_not_set_explicitly)
+ASYNC_TEST_F(async_auto_reset_event_test, Starts_as_not_set_explicitly)
 {
     async_auto_reset_event<> event(false);
-    ASSERT_FALSE(event.is_set());
+    EXPECT_FALSE(event.is_set());
+    co_return;
 }
 
-TEST(async_auto_reset_event_test, Starts_as_set_explicitly)
+ASYNC_TEST_F(async_auto_reset_event_test, Starts_as_set_explicitly)
 {
     async_auto_reset_event<> event(true);
-    ASSERT_TRUE(event.is_set());
+    EXPECT_TRUE(event.is_set());
+    co_return;
 }
 
-TEST(async_auto_reset_event_test, Can_be_reset_after_set)
+ASYNC_TEST_F(async_auto_reset_event_test, Can_be_reset_after_set)
 {
     async_auto_reset_event<> event(true);
     event.reset();
-    ASSERT_FALSE(event.is_set());
+    EXPECT_FALSE(event.is_set());
+    co_return;
 }
 
-TEST(async_auto_reset_event_test, Set_after_await_continues_one_awaiter_in_order_and_leaves_reset)
+ASYNC_TEST_F(async_auto_reset_event_test, Set_after_await_continues_one_awaiter_in_order_and_leaves_reset)
 {
     async_auto_reset_event<> event;
     async_scope<> asyncScope;
+    async_scope<> outerScope;
     bool complete1 = false;
     bool complete2 = false;
     bool complete3 = false;
@@ -60,39 +74,39 @@ TEST(async_auto_reset_event_test, Set_after_await_continues_one_awaiter_in_order
     asyncScope.spawn(waitLambda(complete2));
     asyncScope.spawn(waitLambda(complete3));
 
-    auto future = as_future([&]() -> task<>
-        {
-            co_await asyncScope.join();
-            allComplete = true;
-        });
+    outerScope.spawn([&]() -> task<>
+    {
+        co_await asyncScope.join();
+        allComplete = true;
+    });
 
-    ASSERT_EQ(false, complete1);
-    ASSERT_EQ(false, complete2);
-    ASSERT_EQ(false, complete3);
-    ASSERT_EQ(false, allComplete);
-    ASSERT_EQ(false, event.is_set());
+    EXPECT_EQ(false, complete1);
+    EXPECT_EQ(false, complete2);
+    EXPECT_EQ(false, complete3);
+    EXPECT_EQ(false, allComplete);
+    EXPECT_EQ(false, event.is_set());
     event.set();
-    ASSERT_EQ(true, complete1);
-    ASSERT_EQ(false, complete2);
-    ASSERT_EQ(false, complete3);
-    ASSERT_EQ(false, allComplete);
-    ASSERT_EQ(false, event.is_set());
+    EXPECT_EQ(true, complete1);
+    EXPECT_EQ(false, complete2);
+    EXPECT_EQ(false, complete3);
+    EXPECT_EQ(false, allComplete);
+    EXPECT_EQ(false, event.is_set());
     event.set();
-    ASSERT_EQ(true, complete1);
-    ASSERT_EQ(true, complete2);
-    ASSERT_EQ(false, complete3);
-    ASSERT_EQ(false, allComplete);
-    ASSERT_EQ(false, event.is_set());
+    EXPECT_EQ(true, complete1);
+    EXPECT_EQ(true, complete2);
+    EXPECT_EQ(false, complete3);
+    EXPECT_EQ(false, allComplete);
+    EXPECT_EQ(false, event.is_set());
     event.set();
-    ASSERT_EQ(true, complete1);
-    ASSERT_EQ(true, complete2);
-    ASSERT_EQ(true, complete3);
-    ASSERT_EQ(true, allComplete);
-    future.get();
-    ASSERT_EQ(false, event.is_set());
+    EXPECT_EQ(true, complete1);
+    EXPECT_EQ(true, complete2);
+    EXPECT_EQ(true, complete3);
+    EXPECT_EQ(true, allComplete);
+    co_await outerScope.join();
+    EXPECT_EQ(false, event.is_set());
 }
 
-TEST(async_auto_reset_event_test, Set_before_await_causes_awaiter_to_not_suspend_and_leaves_reset)
+ASYNC_TEST_F(async_auto_reset_event_test, Set_before_await_causes_awaiter_to_not_suspend_and_leaves_reset)
 {
     async_auto_reset_event<> event;
     std::optional<bool> stateBeforeWait;
@@ -101,20 +115,20 @@ TEST(async_auto_reset_event_test, Set_before_await_causes_awaiter_to_not_suspend
 
     event.set();
 
-    auto future = as_future([&]() -> task<>
-        {
-            stateBeforeWait = event.is_set();
-            co_await(suspendResult << event);
-            stateAfterWait = event.is_set();
-        }());
+    auto lambda = [&]() -> task<>
+    {
+        stateBeforeWait = event.is_set();
+        co_await(suspendResult << event);
+        stateAfterWait = event.is_set();
+    };
 
-    future.get();
-    ASSERT_EQ(true, stateBeforeWait);
-    ASSERT_EQ(false, suspendResult.did_suspend());
-    ASSERT_EQ(false, stateAfterWait);
+    co_await lambda();
+    EXPECT_EQ(true, stateBeforeWait);
+    EXPECT_EQ(false, suspendResult.did_suspend());
+    EXPECT_EQ(false, stateAfterWait);
 }
 
-ASYNC_TEST(async_auto_reset_event_test, reentrant_set_leaves_signalled)
+ASYNC_TEST_F(async_auto_reset_event_test, reentrant_set_leaves_signalled)
 {
     async_auto_reset_event<> event;
     auto lambda = [&]() -> task<>
@@ -131,7 +145,7 @@ ASYNC_TEST(async_auto_reset_event_test, reentrant_set_leaves_signalled)
     co_await scope.join();
 }
 
-ASYNC_TEST(async_auto_reset_event_test, can_destroy_from_resumption_of_coroutine)
+ASYNC_TEST_F(async_auto_reset_event_test, can_destroy_from_resumption_of_coroutine)
 {
     async_auto_reset_event<>* innerEventPointer;
 
@@ -147,7 +161,7 @@ ASYNC_TEST(async_auto_reset_event_test, can_destroy_from_resumption_of_coroutine
     co_await scope.join();
 }
 
-ASYNC_TEST(async_auto_reset_event_test, DISABLED_can_loop_without_stack_overflow)
+ASYNC_TEST_F(async_auto_reset_event_test, DISABLED_can_loop_without_stack_overflow)
 {
     async_auto_reset_event<> event;
 
@@ -165,6 +179,53 @@ ASYNC_TEST(async_auto_reset_event_test, DISABLED_can_loop_without_stack_overflow
 
     event.set();
     co_await scope.join();
+}
+
+ASYNC_TEST_F(async_auto_reset_event_test, Many_iterations)
+{
+    for (auto outerCounter = 0; outerCounter < 1000; outerCounter++)
+    {
+        async_auto_reset_event<> event;
+        async_scope<> asyncScope;
+        std::atomic<int> remainingThreads;
+
+        auto waiter = [&](int waiterNumber) -> task<>
+        {
+            ++remainingThreads;
+            for (auto counter = 0; counter < 1000; counter++)
+            {
+                co_await event;
+                co_await waiterPool.schedule();
+            }
+            --remainingThreads;
+
+            //std::cerr << "waiter " << waiterNumber << " done.\n";
+        };
+
+        auto setter = [&](int setterNumber) -> task<>
+        {
+            while (remainingThreads.load())
+            {
+                event.set();
+                co_await setterPool.schedule();
+            }
+
+            //std::cerr << "setter " << setterNumber << " done.\n";
+        };
+
+        for (auto counter = 0; counter < 50; counter++)
+        {
+            asyncScope.spawn(waiter(counter));
+        }
+
+        for (auto counter = 0; counter < 50; counter++)
+        {
+            asyncScope.spawn(setter(counter));
+        }
+
+        co_await asyncScope.join();
+        //std::cerr << "All done!\n";
+    }
 }
 
 }
