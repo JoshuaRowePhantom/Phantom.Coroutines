0d3bed03d90d2b586f165bcafe21ba08556270a3
Joshua Rowe <j.git@phantom.to>
2023-03-06 02:51:52 -0800

    Add PHANTOM_COROUTINES_SYMMETRIC_TRANSFER_INCORRECTLY_LIFTED_TO_COROUTINE_FRAME

--- Phantom.Coroutines/include/Phantom.Coroutines/detail/config.h
+++ Phantom.Coroutines/include/Phantom.Coroutines/detail/config.h
@@ -7,4 +7,9 @@
 #define PHANTOM_COROUTINES_FUTURE_DOESNT_ACCEPT_NOT_DEFAULT_CONSTRUCTIBLE 1
 #endif
 
+// Bug https://developercommunity.visualstudio.com/t/Incorrect-code-generation-for-symmetric/1659260?q=%22symmetric+transfer%22
+#ifndef PHANTOM_COROUTINES_SYMMETRIC_TRANSFER_INCORRECTLY_LIFTED_TO_COROUTINE_FRAME
+#define PHANTOM_COROUTINES_SYMMETRIC_TRANSFER_INCORRECTLY_LIFTED_TO_COROUTINE_FRAME 1
+#endif
+
 #endif
