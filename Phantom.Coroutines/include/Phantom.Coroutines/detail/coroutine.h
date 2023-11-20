7ab855b015eb72b9c5a9f4a301faea338cc71082
Joshua Rowe <jrowe@phantom.to>
2023-11-20 07:02:06 -0800

    Eliminate release mode compiler warnings

--- Phantom.Coroutines/include/Phantom.Coroutines/detail/coroutine.h
+++ Phantom.Coroutines/include/Phantom.Coroutines/detail/coroutine.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <assert.h>
 #include <coroutine>
+#include <tuple>
 
 namespace Phantom::Coroutines
 {
@@ -13,7 +14,7 @@ using coroutine_handle = std::coroutine_handle<TPromise>;
 using suspend_always = std::suspend_always;
 using suspend_never = std::suspend_never;
 
-static inline auto noop_coroutine()
+inline auto noop_coroutine()
 {
     return std::noop_coroutine();
 }
@@ -30,7 +31,7 @@ static auto invalid_coroutine_handle()
 template<
     typename Promise
 >
-static auto copy_and_invalidate(
+auto copy_and_invalidate(
     coroutine_handle<Promise>& handle
 )
 {
@@ -41,18 +42,18 @@ static auto copy_and_invalidate(
     return result;
 }
 
-static bool is_valid(
+inline bool is_valid(
     coroutine_handle<> coroutine
 )
 {
     return coroutine && coroutine != invalid_coroutine_handle();
 }
 
-__pragma(warning(suppress:4505))
-static void assert_is_valid(
+inline void assert_is_valid(
     coroutine_handle<> coroutine
 )
 {
+    std::ignore = coroutine;
     assert(is_valid(coroutine));
 }
 }
